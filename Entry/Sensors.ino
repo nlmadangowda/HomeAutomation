90aba2404e0703616cebba8c63380fc2b0fee641
Madan Kumar N L <71085343+nlmadangowda@users.noreply.github.com>
2022-01-16 13:30:36 +0530

    Added DHT11 & BH1750 interface

---
+++ Entry/Sensors.ino
@@ -0,0 +1,103 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <BH1750.h>
+/*MACROS----------------------------------------------------------------------------------*/
+
+/*MACROS----------------------------------------------------------------------------------*/
+#define DHT_PIN_DI 2
+#define DHTTYPE    DHT11
+
+/*GLOBEL VARIABLES----------------------------------------------------------------------------------*/
+DHT_Unified dht(DHT_PIN_DI, DHTTYPE);
+BH1750 lightMeter(0x23);
+uint32_t delayMS;
+
+void Setup_DHT(){
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  sensor_t sensor;
+  
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  delayMS = sensor.min_delay / 1000;
+}
+
+void Setup_BH1750(){
+  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+    Serial.println(F("BH1750 Advanced begin"));
+  }
+  else {
+    Serial.println(F("Error initialising BH1750"));
+  }
+}
+
+void StartSensors(){
+    Setup_DHT();
+    
+    Wire.begin();
+    Setup_BH1750();
+}
+
+void Read_DHT11(){
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }  
+}
+
+
+void Read_BH1750(){
+  if (lightMeter.measurementReady()) {
+    float lux = lightMeter.readLightLevel();
+    Serial.print("Light: ");
+    Serial.print(lux);
+    Serial.println(" lx");
+  }
+}
+
+
+void Task3(void *pvParameters){
+  (void) pvParameters;
+  while(1){
+    delay(500);
+    Read_BH1750();
+    delay(500);
+    Read_DHT11();
+  }
+}
