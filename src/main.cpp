a842681eaa385eee3e6bdb9207eb20d2bbb6068a
nlmadangowda <madannl17@gmail.com>
2022-03-20 22:10:50 +0530

    add Thigspeak feature

--- src/main.cpp
+++ src/main.cpp
@@ -25,43 +25,9 @@ char default_config[]={
 
 /*GLOBEL VARIABLES----------------------------------------------------------------------------------*/
 DevConfig g_dev_config;
+static int GetDevConfig(){
 
-int UpdateDevConfig(){
-  StaticJsonDocument<360> doc;
-  DeserializationError error = deserializeJson(doc, default_config);
-  if (error) {
-    Serial.print("deserializeMsgPack() failed: ");
-    Serial.println(error.f_str());
-    return 0;
-  }
-  Serial.println("Updating Device Config");
-  g_dev_config.confg_status = doc["confg_status"];
-  const char * doc_mobile_num = doc["mobile_num"];
-  const char * doc_thingspeak_apikey = doc["thingspeak_apikey"];
-  g_dev_config.thingspeak_c_id = doc["thingspeak_c_id"];
-  const char * doc_wifi_ssid = doc["wifi_ssid"];
-  const char * doc_wifi_pass = doc["wifi_pass"];
-  if(strlen(doc_mobile_num)<=sizeof(g_dev_config.mobile_num)){
-    memcpy(&g_dev_config.mobile_num[0],doc_mobile_num,strlen(doc_mobile_num));
-  }
-  if(strlen(doc_thingspeak_apikey)<=sizeof(g_dev_config.thingspeak_apikey)){
-    memcpy(&g_dev_config.thingspeak_apikey[0],doc_thingspeak_apikey,strlen(doc_mobile_num));
-  }
-  if(strlen(doc_wifi_ssid)<=sizeof(g_dev_config.wifi_ssid)){
-    memcpy(&g_dev_config.wifi_ssid[0],doc_wifi_ssid,strlen(doc_wifi_ssid));
-  }
-  if(strlen(doc_wifi_pass)<=sizeof(g_dev_config.wifi_pass)){
-    memcpy(&g_dev_config.wifi_pass[0],doc_wifi_pass,strlen(doc_wifi_pass));
-  }
-  Serial.println(g_dev_config.mobile_num);
-  Serial.println(g_dev_config.wifi_pass);
-  Serial.println(g_dev_config.wifi_ssid);
-  Serial.println(g_dev_config.thingspeak_apikey);
-  Serial.println(g_dev_config.thingspeak_c_id);
-  Serial.println(g_dev_config.confg_status);
-  return 1;
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -72,12 +38,12 @@ void setup() {
   // if(UpdateDevConfig()!=1){
   //   Serial.println("\nFailed to update teh device config, plz reconfigure the settings");
   // }
-
+g_dev_config.confg_status = 1;
   if( g_dev_config.confg_status == 1){
     StartWiFi();
-    StartSensors();
+    // StartSensors();
     xTaskCreatePinnedToCore(WifiTask, "WifiTask", 4096, NULL, 2, NULL, 1);
-    xTaskCreatePinnedToCore(SensorTask, "SensorTask", 2048, NULL, 2, NULL, 0);
+    // xTaskCreatePinnedToCore(SensorTask, "SensorTask", 2048, NULL, 2, NULL, 0);
   }else{
     StartBLE();
     xTaskCreatePinnedToCore(BLETask, "BLETask", 2048, NULL, 1, NULL, 1);
--- src/wifi.cpp
+++ src/wifi.cpp
@@ -1,19 +1,47 @@
-//#include <WiFi.h>
 #include "main.h"
-
 #include <WiFiMulti.h>
+#include "ThingSpeak.h"
+
+
 WiFiMulti wifiMulti;
+WiFiClient  client;
+
+unsigned long myChannelNumber = 1680155;
+const char * myWriteAPIKey = "7TXEYK6XZO446CJZ";
+String myStatus = "";
 
 void WifiTask(void *pvParameters){
   (void) pvParameters;
+  int number1= 0;
   while(1){
     if(wifiMulti.run() != WL_CONNECTED) {
         Serial.println("WiFi not connected!");
-        delay(500);
-    }else{
-      Serial.println("Task1");
-      delay(100);
+        delay(1000);
     }
+    // set the fields with the values
+    // ThingSpeak.setField(1, number1);
+    // figure out the status message
+    // myStatus = String("field1 equals field2");
+    
+    // set the status
+    // ThingSpeak.setStatus(myStatus);
+    
+    // write to the ThingSpeak channel
+    int x = ThingSpeak.writeField(myChannelNumber, 1, number1, myWriteAPIKey);
+    if(x == 200){
+      Serial.println("Channel update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+    
+    // change the values
+    number1++;
+    if(number1 > 99){
+      number1 = 0;
+    }
+    
+    delay(10000); // Wait 20 seconds to update the channel again
   }
 }
 
@@ -29,4 +57,5 @@ void StartWiFi(){
       Serial.println("IP address: ");
       Serial.println(WiFi.localIP());
     }
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
 }
